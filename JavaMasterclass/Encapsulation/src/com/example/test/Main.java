f9293ba4c7ddc3100d13593c25013ef2af436747
stomaras <sptoma724@gmail.com>
2021-06-01 12:58:03 +0300

    Encapsulation

---
+++ JavaMasterclass/Encapsulation/src/com/example/test/Main.java
@@ -0,0 +1,10 @@
+package com.example.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Player player = new Player("Tom", 50, "Sword");
+        System.out.println("Initial health is " + player.getHealth());
+    }
+}
