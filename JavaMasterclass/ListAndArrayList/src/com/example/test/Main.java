51541f6e1aa97006c7ea844bbc284e4a4660c4f9
stomaras <sptoma724@gmail.com>
2021-06-03 11:42:00 +0300

    ArrayList

---
+++ JavaMasterclass/ListAndArrayList/src/com/example/test/Main.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import java.util.Scanner;
+// ArrayList inherits from the list which actually inherits from an abstract list
+// ArrayList handles the resizing automatically
+public class Main {
+
+
+    public static void main(String[] args) {
+    }
+
+
+}
