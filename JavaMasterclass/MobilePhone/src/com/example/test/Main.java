874753c9213bb4682167eb7dbdbe3085ca69176f
stomaras <sptoma724@gmail.com>
2021-06-03 17:11:52 +0300

    MobilePhone Part 2

---
+++ JavaMasterclass/MobilePhone/src/com/example/test/Main.java
@@ -0,0 +1,122 @@
+package com.example.test;
+
+import java.util.Scanner;
+
+//Create a program that implements a simple mobile phone with the following capabilities.
+//
+//1. Implement the master class MobilePhone, that holds the ArrayList of Contacts,
+// with the following attributes
+// Two fields, a String called myNumber and an ArrayList of Type Contact called
+// myContacts.
+// A constructor that takes a String(the phone number) and initialises myNumber and
+// instantiates myContacts
+// And seven methods, they are(their functions are in their names):
+// - addNewContact(), has one parameter of type Contact and returns a boolean.
+// Returns true if the contact does not exist, or false if the contact already exists.
+// - updateContact(), has two parameters of type Contact(the old contact that will be
+// updated with the new contact) and returns a boolean.
+// - removeContact(), has one parameter of type Contact and returns a boolean. Returns
+// true if the contact exists and was removed successfully, or false if the contact
+// does not exists.
+// - findContact(), has one parameter of type Contact and returns an int. The returned
+// value it's position in the ArrayList, it will either be -1(does not exists) or a value
+// greater than or equal to 0(does exists).
+// - findContact(), same as above, only it has one parameter of type String.
+// - queryContact(), has one parameter of type String and returns a Contact.Use the
+// String to search for the name and then return the Contact. Return null otherwise.
+// - printContacts(), has no parameters and does not return anything. Print the contacts
+// in the following format:
+// Contact List:
+// 1. Bob -> 314684389
+// 2. Alice -> 16180339
+// 3. Tom -> 112345788
+// Implement the Contact class with the following attributes:
+// Two fields, both String, one called name and the other phoneNumber.
+// A constructor that takes two String's and initialises name and phone number.
+// And Three methods, they are:
+// getName(), getter for name.
+// getPhoneNumber(), getter for phoneNumber
+// createContact(), has two parameters of type String (the persons name and phone number)
+// and returns an instance of Contact. This is the only method that is static.
+public class Main {
+
+    private static Scanner sc = new Scanner(System.in);
+    private static MobilePhone mobilePhone = new MobilePhone("0039 330 4404");
+
+    public static void main(String[] args) {
+	// write your code here
+
+        boolean quit = false;
+        startPhone();
+        printActions();
+        while (!quit) {
+
+            System.out.println("\nEnter action: 6 to show available actions");
+            int action = sc.nextInt();
+            sc.nextLine();
+
+            switch (action) {
+                case 0:
+                    System.out.println("\nShutting down...");
+                    quit = true;
+                    break;
+                case 1:
+                    mobilePhone.printContacts();
+                    break;
+                case 2:
+                    addNewContact();
+                    break;
+                case 3:
+                    //updateContact();
+                    break;
+                case 4:
+                    //removeContact();
+                    break;
+                case 5:
+                    //queryContact();
+                    break;
+                case 6:
+                    printActions();
+                    break;
+            }
+
+
+        }
+
+
+
+
+    }
+
+
+    private static void addNewContact(){
+        System.out.println("Enter new contact name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter phone number");
+        String phone = sc.nextLine();
+        Contact newContact = Contact.createContact(name, phone);
+        if (mobilePhone.addNewContact(newContact)){
+            System.out.println("New contact added: name = " + name + ", phone = " +phone);
+        }else {
+            System.out.println("Cannot add, " + name + " already on file ");
+        }
+    }
+
+
+    private static void startPhone() {
+        System.out.println("Starting phone...");
+    }
+
+    private static void printActions() {
+        System.out.println("\nAvailable actions: \npress");
+        System.out.println("0 - to shutdown\n" +
+                "1 - to print contacts\n" +
+                "2 - to add a new contact\n" +
+                "3 - to update existing contact\n" +
+                "4 - to remove an existing contact\n" +
+                "5 - query if an existing contact exists\n" +
+                "6 - to print a list of available actions."
+                );
+        System.out.println("Choose your action: ");
+    }
+}
---
+++ JavaMasterclass/MobilePhone/src/com/example/test/MobilePhone.java
@@ -0,0 +1,109 @@
+package com.example.test;
+
+import java.util.ArrayList;
+
+public class MobilePhone {
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
+    }
+
+    // 0. Print Instructions
+    public void printInstructions(){
+        System.out.println(" Choice 0: printInstructions \n" +
+                "Choice 1: add new Contact \n" +
+                "Choice 2: Update Contact \n" +
+                "Choice 3: Remove Contact \n" +
+                "Choice 4: Find Contact \n" +
+                "Choice 5: Query Contact \n" +
+                "Choice 6: Print Contacts");
+    }
+
+    // 1. Add new contact
+    public boolean addNewContact(Contact contact){
+        if (findContact(contact.getName()) >= 0) {
+            return false;
+        }
+        myContacts.add(contact);
+        return true;
+    }
+
+
+
+    // 2. Update oldContact with a newContact
+    public boolean updateContact(Contact oldContact, Contact newContact) {
+        int position = findContact(oldContact);
+        if (position >= 0){
+            myContacts.set(position, newContact);
+            System.out.println("Contact exists and updated successfully!");
+            return true;
+        } else {
+            System.out.println("Contact does not exists!");
+            return false;
+        }
+    }
+
+    // 3. Remove contact
+    public boolean removeContact(Contact contact) {
+        int position = findContact(contact);
+        if (position < 0){
+            System.out.println("Contact does not exists");
+            return false;
+        } else {
+            myContacts.remove(position);
+            System.out.println("Contact removed successfully!");
+            return true;
+        }
+    }
+
+    // 4. Find Contact
+    //////////////////////////////////////////////
+    //////////////////////////////////////////////
+    private int findContact(Contact contact) {
+        int position = myContacts.indexOf(contact);
+        if (position >= 0) {
+            return position;
+        } else {
+            return -1;
+        }
+    }
+
+    private int findContact(String contactName) {
+        for (int i = 0; i< this.myContacts.size(); i++) {
+            Contact contact = this.myContacts.get(i);
+            if (contact.getName().equals(contactName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    /////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////
+
+    // 5. Query Contact
+    public String queryContact(Contact contact){
+        if (findContact(contact) >= 0){
+            return contact.getName();
+        }
+        return null;
+    }
+
+    // 6. Print Contacts
+    public void printContacts(){
+        System.out.println("Contact List: \n");
+        for (int i = 0; i < myContacts.size(); i++) {
+            System.out.println(+ i + ". " + myContacts.get(i).getName() + " -> " + myContacts.get(i).getPhoneNumber());
+        }
+    }
+
+
+
+
+
+
+
+
+}
