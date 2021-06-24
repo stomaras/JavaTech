12761f96b790ecfaefac0994ce9e23d4c1b21ba0
stomaras <sptoma724@gmail.com>
2021-06-24 10:15:34 +0300

    Finishing off Sets

--- JavaMasterclass/SetAndHashSet/src/com/example/test/SetMain.java
+++ JavaMasterclass/SetAndHashSet/src/com/example/test/SetMain.java
@@ -9,7 +9,7 @@ public class SetMain {
         Set<Integer> squares = new HashSet<>();
         Set<Integer> cubes = new HashSet<>();
 
-        for (int i=1; i <= 100; i++) {
+        for (int i = 1; i <= 100; i++) {
             squares.add(i * i);
             cubes.add(i * i * i);
         }
@@ -50,7 +50,69 @@ public class SetMain {
         System.out.println(nature);
         System.out.println(divine);
 
+        System.out.println("nature-divine");
+        Set<String> diff1 = new HashSet<>(nature); // we initialize with the nature set
+        diff1.removeAll(divine);
+        printSet(diff1);
+
+        System.out.println("divine-nature");
+        Set<String> diff2 = new HashSet<>(divine);
+        diff2.removeAll(nature);
+        printSet(diff2);
+
+        // Union
+        Set<String> unionTest = new HashSet<>(nature);
+        unionTest.addAll(divine);
+        Set<String> intersectionTest = new HashSet<>(nature);
+        intersectionTest.retainAll(divine);
+        // Symmetric difference is just removing the intersection from
+        // the union so to do that
+        System.out.println("Symmetric difference");
+        unionTest.removeAll(intersectionTest);
+        printSet(unionTest);
+
+        // contains all is used to test it if one set is a super set of another
+        if (nature.containsAll(divine)) {
+            System.out.println("divine is a subset of nature");
+        }
+
+        if (nature.containsAll(intersectionTest)) {
+            System.out.println("Intersection is subset of nature");
+        }
+
+        if (divine.containsAll(intersectionTest)) {
+            System.out.println("intersection is a subset of divine");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
+
+    private static void printSet(Set<String> set) {
+        System.out.print("\t");
+        for (String s : set) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+    }
+
+    /*
+    Symmetric difference is the elements that appear in one set or the other but not both
+    so it can therefore be defined as the union minus the intersection
+
+     */
+
 }
+
