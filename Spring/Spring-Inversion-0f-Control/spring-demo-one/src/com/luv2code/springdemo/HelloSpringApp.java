e3e7b21cd703c9bfd7e7af17c1d971d21641607b
stomaras <sptoma724@gmail.com>
2021-07-08 16:25:43 +0300

    Section 4: Spring Inversion Of Control

---
+++ Spring/Spring-Inversion-0f-Control/spring-demo-one/src/com/luv2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,23 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+	
+	public static void main(String[] args) {
+		
+		// load the Spring configuration file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// retrieve a bean from the Spring Container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		// call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		// close the context
+		context.close();
+	}
+
+}
