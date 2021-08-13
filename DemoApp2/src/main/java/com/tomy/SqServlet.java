a54a0fc8dcc47f6409f128ad4e004d5dfc579537
stomaras <sptoma724@gmail.com>
2021-08-13 13:58:56 +0300

    Session Management and send data from one servlet to another with session management technique

---
+++ DemoApp2/src/main/java/com/tomy/SqServlet.java
@@ -0,0 +1,29 @@
+package com.tomy;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SqServlet extends HttpServlet 
+{
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException 
+	{
+		HttpSession session = req.getSession();
+		
+		// cast in order to convert object to int
+		int k = (int) session.getAttribute("k");
+		
+		k = k*k;
+		
+		PrintWriter out = res.getWriter();
+		out.println("Result is " + k);
+		
+		out.println("sq called");
+	}
+
+}
