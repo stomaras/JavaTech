819649873b7a92442ff6ab3612356acf2a1c11bb
stomaras <sptoma724@gmail.com>
2021-08-14 15:10:48 +0300

    Servlet Annotation Configuration

---
+++ ServletConfigurationAnnotation/src/main/java/com/tom/AddServlet.java
@@ -0,0 +1,36 @@
+package com.tom;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/add")
+public class AddServlet extends HttpServlet
+{
+	
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+	{
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j;
+		
+		HttpSession session = req.getSession();
+		session.setAttribute("k", k);
+		
+		res.sendRedirect("sq"); 
+
+	}
+
+}
+
