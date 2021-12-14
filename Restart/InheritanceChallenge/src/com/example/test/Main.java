10fb86a27bd02946edb67ac08a2dcd916b420922
stomaras <sptoma724@gmail.com>
2021-12-14 16:54:50 +0200

    Inheritance

---
+++ Restart/InheritanceChallenge/src/com/example/test/Main.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // Challenge
+        // Start with a base class of a Vehicle, then create a Car class that inherits from this base class.
+        // Finally, create another class, a specific type of Car that inherits from the Car class.
+        // You should be able to hand steering, changing gears, and moving (speed in other words).
+        // As i mentioned above, changing gears, increase speed should be included.
+        // For you specific type of vehicle you will want to add something specific for that type of car.
+    }
+}
