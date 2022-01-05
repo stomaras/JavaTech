3290f27ecf17d85809bf7ab8637ff4a2ffc7c238
stomaras <sptoma724@gmail.com>
2022-01-05 13:11:46 +0200

    Collection and Generics Explain

---
+++ Restart/CollectionAndGenericsExplain/src/com/example/test/Main.java
@@ -0,0 +1,37 @@
+package com.example.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // collection - > topic
+        // Collection -> Interface
+        // Collections -> Class
+        // Collection is a interface -> List is an Interface -> ArrayList is a class
+        // Collection values = new ArrayList(); , can save any type of String, integers
+        // If you have a specific type you use generics
+        // <> THIS TYPE OF BRACKETS used for generics
+        // Collection<Integer> values = new ArrayList<Integer>();
+
+        // List<Integer> values = new ArrayList<>();
+        // Set<Integer> numbers = new HashSet<>();
+        // Set<Integer> numbers = new TreeSet<>();
+        //
+        // in List Interface you have duplicate values
+        // in Set Interface you don't have duplicate values
+        // in List you traverse numbers in sequence access
+        // in Set Interface you have random access in elements
+        // in TreeSet you have access in sequence order.
+        // We have a lot of interfaces a lot of classes in a big collection API
+        // In a List interface we can have duplicate elements and every element
+        // will have a unique index number.
+        // Map will not have a number will have a key Map<k,v> , for every value
+        // will have a key, the key can be a number it can be a string it can be anything
+        // Map is also an interface now we have to search for that class which implements
+        // that interface and that class is HashMap
+        // We have two class here which implements Map Interface. One is HashMap and second
+        // is HashTable, one is synchronized one is not you can go for a thread safe implementation
+        // or a non thread safe implementation.
+
+    }
+}
