39076eae1df29873e170aa6b09ca80b8a9a6271e
stomaras <sptoma724@gmail.com>
2022-01-20 12:26:17 +0200

    Sets Symmetric and Asymmetric

---
+++ Restart/SetsSymmetricAndAsymmetric/src/com/example/test/SetMain.java
@@ -0,0 +1,66 @@
+package com.example.test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetMain {
+    public static void main(String[] args) {
+        Set<Integer> squares = new HashSet<>();
+        Set<Integer> cubes = new HashSet<>();
+
+        for (int i=1; i <= 100; i++) {
+            squares.add(i * i);
+            cubes.add(i * i * i);
+        }
+
+        System.out.println("There are " + squares.size() + " squares and " + cubes.size() + " cubes");
+        Set<Integer> union = new HashSet<>(squares);
+        union.addAll(cubes);
+        System.out.println();
+        System.out.println("Union contains " + union.size() + " elements.");
+        System.out.println();
+        Set<Integer> intersection = new HashSet<>(squares);
+        intersection.retainAll(cubes);
+        System.out.println("Intersection contains " + intersection.size() + " elements.");
+        for (int i : intersection) {
+            System.out.println(i + " is the square of " + Math.sqrt(i) + " and the cube of " + Math.cbrt(i));
+        }
+        System.out.println("==================================");
+        Set<String> words = new HashSet<>();
+        String sentence = "one day in the year of the fox";
+        String[] arrayWords = sentence.split(" ");
+        words.addAll(Arrays.asList(arrayWords));
+
+        for (String s : words) {
+            System.out.println(s);
+        }
+
+        Set<String> nature = new HashSet<>();
+        Set<String> divine = new HashSet<>();
+        String[] natureWords = {"all", "nature", "is", "but", "art", "unknown", "to", "the"};
+        nature.addAll(Arrays.asList(natureWords));
+        String[] divineWords = {"to", "err", "is", "human", "to", "forgive", "divine"};
+        divine.addAll(Arrays.asList(divineWords));
+
+        System.out.println("nature - divine: ");
+        Set<String> diff1 = new HashSet<>(nature);
+        diff1.removeAll(divine);
+        printSet(diff1);
+
+        System.out.println("divine - nature: ");
+        Set<String> diff2 = new HashSet<>(divine);
+        diff2.removeAll(nature);
+        printSet(diff2);
+
+
+    }
+
+    private static void printSet(Set<String> set) {
+        System.out.println("\t");
+        for (String s : set) {
+            System.out.println(s + " ");
+        }
+        System.out.println();
+    }
+}
