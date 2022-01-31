284fbe8c956001d550d4fa56bca8550f9e14fa4d
stomaras <sptoma724@gmail.com>
2022-01-31 18:01:19 +0200

    Encapsulation Challenege Start Printer use case

---
+++ AllChallenges/Challenge-Encapsulation/EncapsulationChallengePrinter/src/com/example/test/Main.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	In this exercise you will create one class and name it Printer. This class will have two member variables of type
+	int, tonerLevel and pagesPrinted, and one of type boolean called duplex. All three fields will have private access
+	. The constructor will accept two of these variables, tonerLevel and duplex, as parameters following these rules:
+	    - tonerLevel: must be greater than -1 but less than or equal to 100. If it does not meet these conditions
+	      then it should be initialized to -1.
+	    - duplex: should be initialized to the value of the parameter.
+	 */
+    }
+}
