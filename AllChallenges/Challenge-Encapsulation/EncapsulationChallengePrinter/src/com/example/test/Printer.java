44d2bd807281af5a22370f3ec1ca4c56b28f94af
stomaras <sptoma724@gmail.com>
2022-02-01 17:22:59 +0200

    Printer encapsulation

---
+++ AllChallenges/Challenge-Encapsulation/EncapsulationChallengePrinter/src/com/example/test/Printer.java
@@ -0,0 +1,47 @@
+package com.example.test;
+
+public class Printer {
+
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        if ((tonerLevel > -1) && (tonerLevel <= 100)) {
+            this.tonerLevel = tonerLevel;
+        } else {
+            this.tonerLevel = -1;
+        }
+        this.duplex = duplex;
+        this.pagesPrinted = 0;
+    }
+
+    public int addToner(int tonerAmount){
+        if ((tonerAmount > 0) && (tonerAmount <= 100)) {
+            if ((tonerLevel + tonerAmount) > 100){
+                return -1;
+            } else {
+                tonerLevel += tonerAmount;
+                return tonerLevel;
+            }
+        } else {
+            return -1;
+        }
+    }
+
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+        if (duplex == true){
+            pagesToPrint *= 2;
+            pagesPrinted += pagesToPrint;
+            return pagesToPrint;
+        } else {
+            pagesPrinted += pagesToPrint;
+            return pagesToPrint;
+        }
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+}
