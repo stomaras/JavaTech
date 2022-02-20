644e4f0fca0c03d45bd34836a9a2249febd44fde
stomaras <sptoma724@gmail.com>
2022-02-20 13:01:27 +0200

    Reverse Array

--- AllChallenges/Arrays,Java,Inbuilt Lists, Autboxing, Unboxing/ReverseArray/ReverseArrayChallenge/src/com/example/test/ReverseArray.java
+++ AllChallenges/Arrays,Java,Inbuilt Lists, Autboxing, Unboxing/ReverseArray/ReverseArrayChallenge/src/com/example/test/ReverseArray.java
@@ -2,9 +2,27 @@ package com.example.test;
 
 public class ReverseArray {
     public static void main(String[] args) {
-
+        int[] array = new int[] {1,2,3,4,5};
+        reverse(array, 5);
 
     }
+
+    static void reverse(int a[], int n)
+    {
+        int i, k, t;
+        for (i = 0; i < n / 2; i++) {
+            t = a[i];
+            a[i] = a[n - i - 1];
+            a[n - i - 1] = t;
+        }
+
+        // printing the reversed array
+        System.out.print("Reversed array is: \n");
+        for (k = 0; k < n; k++) {
+            System.out.print(a[k]);
+        }
+    }
+
 
 
 }
