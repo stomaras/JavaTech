ad9873e61322febf7b2bf8e1624d0886418586f6
stomaras <sptoma724@gmail.com>
2022-03-14 22:22:44 +0200

    String constructors todo String methods

---
+++ Restart/String-StringBuffer-StringBuilder/StringStringBufferStringBuilderPart1/src/com/example/test/case5.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+public class case5 {
+
+    public static void main(String[] args) {
+        char[] ch = {'j','a','v','a'};
+        String s = new String(ch);
+        System.out.println(s);
+
+        byte[] b = {97,98,99,100};
+        String s1 = new String(b);
+        System.out.println(s1);
+    }
+}
---
+++ Restart/String-StringBuffer-StringBuilder/StringStringBufferStringBuilderPart1/src/com/example/test/case6.java
@@ -0,0 +1,11 @@
+package com.example.test;
+
+public class case6 {
+
+    public static void main(String[] args) {
+        String s = new String("Tomy");
+        System.out.println(s.charAt(3));
+        //System.out.println(s.charAt(45));
+        //StringIndexOutOfBoundsException
+    }
+}
